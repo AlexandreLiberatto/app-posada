0d5286b3873965ba5a0a8fbe4267e59eed1504d9
Alexandre liberato <alexandreliberatto@gmail.com>
2024-08-23 09:32:17 -0300

    Criando estrutura do AppNavigator.js

---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from '../screens/LoginScreen';
+import RoomsScreen from '../screens/RoomsScreen';
+import BookingScreen from '../screens/BookingScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Rooms" component={RoomsScreen} />
+      <Stack.Screen name="Booking" component={BookingScreen} />
+    </Stack.Navigator>
+  );
+}
